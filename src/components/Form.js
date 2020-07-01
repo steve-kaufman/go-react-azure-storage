eeb01970d60b46d9ba0a5d143ea73bc1f395229d
Steven Kaufman <steveplaysguiola@gmail.com>
2020-07-01 12:15:18 -0500

    added a form for file version

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,31 @@
 import React, { useContext } from "react"
+import "./Form.sass"
 import { Icon } from "@iconify/react"
 import uploadIcon from "@iconify/icons-mdi/upload"
+
 import { UploaderContext } from "../context/UploaderContext"
-import "./Form.sass"
+import { useInput } from "../hooks/useInput"
 
 export const Form = () => {
   const uploader = useContext(UploaderContext)
 
+  const [fileVersion, setFileversion] = useInput()
+
   const onSubmit = (e) => {
     e.preventDefault()
+    uploader.methods.setParams({ fileVersion })
     uploader.methods.uploadStoredFiles()
   }
   return (
     <form id="uploader-form" onSubmit={onSubmit}>
+      <div className="input-group">
+        <label htmlFor="file-version">File Version</label>
+        <input
+          name="file-version"
+          value={fileVersion}
+          onChange={setFileversion}
+        />
+      </div>
       <button type="submit">
         Upload <Icon icon={uploadIcon} />
       </button>
